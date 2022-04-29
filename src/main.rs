accdeb9597d5e6f44e6a87923278686d545bfcc0
Aleksander Heintz <alxandr@alxandr.me>
2022-04-29 22:14:39 +0200

    Make code more presentation friendly

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,18 @@
+mod prelude;
 mod secret_types;
 
-use color_eyre::Result;
-use futures::StreamExt;
-use k8s_openapi::{api::core::v1::Secret, ByteString};
-use kube::{
-  api::{ListParams, Patch, PatchParams},
-  core::ObjectMeta,
-  runtime::{
-    controller::{Action, Context},
-    Controller,
-  },
-  Api, Client, CustomResource, CustomResourceExt, Resource,
-};
-use schemars::JsonSchema;
-use secret_types::AutoSecretType;
-use serde::{Deserialize, Serialize};
-use std::{
-  collections::{BTreeMap, HashMap},
-  hash::{Hash, Hasher},
-  io::BufRead,
-  sync::Arc,
-  time::Duration,
-};
-use thiserror::Error;
-use tracing::{debug, info, warn};
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
-use tracing_tree::HierarchicalLayer;
+use prelude::*;
 
 #[derive(CustomResource, Debug, Clone, Deserialize, Serialize, JsonSchema)]
 #[kube(group = "webstep.no", version = "v1alpha1", kind = "AutoSecret")]
 #[kube(shortname = "as", namespaced)]
-struct AutoSecretSpec {
+pub struct AutoSecretSpec {
   secrets: HashMap<String, AutoSecretType>,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  color_eyre::install()?;
-  // 👇 new!
-  Registry::default()
-    .with(EnvFilter::from_default_env())
-    .with(HierarchicalLayer::new(2).with_targets(true).with_bracketed_fields(true))
-    .init();
+  setup_logging()?;
 
   let args = argwerk::args! {
     /// auto-secret controller
@@ -82,26 +53,11 @@ async fn main() -> Result<()> {
   info!("starting autosecret-controller");
   info!("press <enter> to force a reconciliation of all objects");
 
-  let (mut reload_tx, reload_rx) = futures::channel::mpsc::channel(0);
-  // Using a regular background thread since tokio::io::stdin() doesn't allow aborting reads,
-  // and its worker prevents the Tokio runtime from shutting down.
-  std::thread::spawn(move || {
-    for _ in std::io::BufReader::new(std::io::stdin()).lines() {
-      let _ = reload_tx.try_send(());
-    }
-  });
-
   Controller::new(autosecrets, ListParams::default())
     .owns(secrets, ListParams::default())
-    .reconcile_all_on(reload_rx.map(|_| ()))
-    .shutdown_on_signal()
+    .handle_signals()
     .run(reconcile, error_policy, Context::new(client))
-    .for_each(|res| async move {
-      match res {
-        Ok((o, _)) => info!("reconciled {}/{}", o.namespace.as_deref().unwrap_or("NIL"), o.name),
-        Err(e) => warn!("reconcile failed: {}", e),
-      }
-    })
+    .for_each(log_reconciler_result)
     .await;
 
   info!("controller terminated");
@@ -115,138 +71,43 @@ async fn main() -> Result<()> {
 ))]
 async fn reconcile(resource: Arc<AutoSecret>, ctx: Context<Client>) -> Result<Action, ControllerError> {
   let client = ctx.get_ref().clone();
-  let namespace = resource
-    .metadata
-    .namespace
-    .as_deref()
-    .ok_or(ControllerError::MissingObjectKey(".metadata.namespace"))?;
 
-  let name = resource
-    .metadata
-    .name
-    .as_deref()
-    .ok_or(ControllerError::MissingObjectKey(".metadata.name"))?;
+  // get existing secret (from k8s) or create new empty (in-memory) secret
+  // with the correct metadata.
+  let mut secret = client.get_secret_or_default(&resource).await?;
 
-  let secret_api = Api::<Secret>::namespaced(client.clone(), namespace);
+  // get secret value pairs from the spec
+  let spec_secrets = resource.secrets();
 
-  let oref = resource.controller_owner_ref(&()).unwrap();
-  let mut secret = Secret {
-    metadata: ObjectMeta {
-      name: Some(name.into()),
-      owner_references: Some(vec![oref]),
-      ..ObjectMeta::default()
-    },
-    ..Default::default()
-  };
+  // remove (in-memory) all secrets from the k8s secret
+  // that does not exist in the spec
+  secret.retain(|name, _| spec_secrets.contains_key(name));
 
-  let existing_secret = get_secret(&secret_api, name).await?;
-  if let Some(existing_secret) = existing_secret {
-    secret.metadata.annotations = existing_secret.metadata.annotations.map(|mut annotations| {
-      annotations.retain(|k, _| k.starts_with(ANNOTATION_PREFIX));
-      annotations
-    });
+  // update or create missing secrets in the k8s secret
+  // that do exist in the spec
+  for (name, secret_spec) in &spec_secrets {
+    match secret.secret_status(name, secret_spec) {
+      SecretStatus::Missing => info!("creating new secret {}", name),
+      SecretStatus::Outdated => info!("updating secret {} due to hash change", name),
+      SecretStatus::Matches => {
+        info!("skipping secret {} due to same hash", name);
+        continue;
+      }
+    }
 
-    secret.data = existing_secret.data;
+    secret.set_secret(name, secret_spec);
   }
 
-  let annotations = secret.metadata.annotations.get_or_insert_with(Default::default);
-  let data = secret.data.get_or_insert_with(Default::default);
-
-  let to_remove: Vec<_> = data
-    .keys()
-    .filter(|name| !resource.spec.secrets.contains_key(&**name))
-    .map(String::from)
-    .collect();
-
-  for name in to_remove {
-    remove_secret(annotations, data, &*name);
-  }
-
-  for (name, conf) in &resource.spec.secrets {
-    check_secret(annotations, data, &**name, conf);
-  }
-
-  patch_secret(secret_api, name, secret).await?;
+  // apply secret in k8s
+  secret.apply(client).await?;
 
   Ok(Action::await_change())
 }
 
-fn check_secret(
-  annotations: &mut BTreeMap<String, String>,
-  data: &mut BTreeMap<String, ByteString>,
-  name: &str,
-  conf: &AutoSecretType,
-) {
-  let annotation_name = annotation_name(name);
-  let expected_hash = annotations.get(&annotation_name).cloned();
-  let actual_hash = hash(conf);
-
-  match expected_hash {
-    Some(expected) if expected == actual_hash => {
-      debug!("skipping secret {} due to same hash", name);
-      return;
-    }
-    Some(_) => info!("updating secret {} due to hash change", name),
-    None => info!("creating new secret {}", name),
-  }
-
-  let value = ByteString(conf.generate().into_bytes());
-  annotations.insert(annotation_name, actual_hash);
-  data.insert(name.into(), value);
-}
-
-fn remove_secret(annotations: &mut BTreeMap<String, String>, data: &mut BTreeMap<String, ByteString>, name: &str) {
-  info!("removing secret {}", name);
-  annotations.remove(&annotation_name(name));
-  data.remove(name);
-}
-
-const ANNOTATION_PREFIX: &str = "autosecrets.webstep.no/";
-
-fn annotation_name(name: &str) -> String {
-  format!("{ANNOTATION_PREFIX}{name}")
-}
-
-#[tracing::instrument(skip_all, fields(secret.name = name))]
-async fn get_secret(secret_api: &Api<Secret>, name: &str) -> Result<Option<Secret>, ControllerError> {
-  secret_api.get_opt(name).await.map_err(ControllerError::SecretGetFailed)
-}
-
-#[tracing::instrument(skip_all, fields(secret.name = name))]
-async fn patch_secret(secret_api: Api<Secret>, name: &str, secret: Secret) -> Result<(), ControllerError> {
-  secret_api
-    .patch(
-      name,
-      &PatchParams::apply("autosecrets.webstep.no").force(),
-      &Patch::Apply(&secret),
-    )
-    .await
-    .map_err(ControllerError::SecretApplyFailed)?;
-
-  Ok(())
-}
+// copy in everything below this line
 
 /// The controller triggers this on reconcile errors
 #[tracing::instrument(skip_all)]
 fn error_policy(_: &ControllerError, _: Context<Client>) -> Action {
   Action::requeue(Duration::from_secs(15))
-}
-
-fn hash(value: &impl Hash) -> String {
-  let mut hasher = seahash::SeaHasher::new();
-  value.hash(&mut hasher);
-  let value = hasher.finish();
-  hex::encode(value.to_le_bytes())
-}
-
-#[derive(Debug, Error)]
-enum ControllerError {
-  #[error("Failed to get secret: {0}")]
-  SecretGetFailed(#[source] kube::Error),
-
-  #[error("Failed to apply secret: {0}")]
-  SecretApplyFailed(#[source] kube::Error),
-
-  #[error("MissingObjectKey: {0}")]
-  MissingObjectKey(&'static str),
 }
---
+++ src/prelude.rs
@@ -0,0 +1,261 @@
+use futures::Stream;
+use kube::runtime::{controller, reflector::ObjectRef, watcher};
+
+pub use super::secret_types::AutoSecretType;
+pub use color_eyre::Result;
+pub use futures::StreamExt;
+pub use k8s_openapi::{api::core::v1::Secret, ByteString};
+pub use kube::{
+  api::{ListParams, Patch, PatchParams},
+  core::ObjectMeta,
+  runtime::{
+    controller::{Action, Context},
+    Controller,
+  },
+  Api, Client, CustomResource, CustomResourceExt, Resource,
+};
+pub use schemars::JsonSchema;
+pub use serde::{Deserialize, Serialize};
+pub use std::{
+  collections::{BTreeMap, HashMap},
+  hash::{Hash, Hasher},
+  io::BufRead,
+  sync::Arc,
+  time::Duration,
+};
+pub use thiserror::Error;
+pub use tracing::{info, warn};
+pub use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
+pub use tracing_tree::HierarchicalLayer;
+
+pub fn setup_logging() -> Result<()> {
+  color_eyre::install()?;
+  Registry::default()
+    .with(EnvFilter::from_default_env())
+    .with(HierarchicalLayer::new(2).with_targets(true).with_bracketed_fields(true))
+    .init();
+
+  Ok(())
+}
+
+pub fn stdin_newlines() -> impl Stream<Item = ()> + Send + Sync {
+  // reconcile everything anew when pressing enter
+  let (mut reload_tx, reload_rx) = futures::channel::mpsc::channel(0);
+  // Using a regular background thread since tokio::io::stdin() doesn't allow aborting reads,
+  // and its worker prevents the Tokio runtime from shutting down.
+  std::thread::spawn(move || {
+    for _ in std::io::BufReader::new(std::io::stdin()).lines() {
+      let _ = reload_tx.try_send(());
+    }
+  });
+
+  reload_rx.map(|_| ())
+}
+
+pub async fn log_reconciler_result(
+  res: Result<(ObjectRef<super::AutoSecret>, Action), controller::Error<ControllerError, watcher::Error>>,
+) {
+  match res {
+    Ok((o, _)) => info!("reconciled {}/{}", o.namespace.as_deref().unwrap_or("NIL"), o.name),
+    Err(e) => warn!("reconcile failed: {}", e),
+  }
+}
+
+pub trait ControllerExt {
+  fn handle_signals(self) -> Self;
+}
+
+impl ControllerExt for Controller<super::AutoSecret> {
+  fn handle_signals(self) -> Self {
+    self.reconcile_all_on(stdin_newlines()).shutdown_on_signal()
+  }
+}
+
+#[async_trait::async_trait]
+pub trait ClientExt {
+  async fn get_secret_or_default(&self, auto_secret: &super::AutoSecret) -> Result<Secret, ControllerError>;
+}
+
+#[async_trait::async_trait]
+impl ClientExt for Client {
+  async fn get_secret_or_default(&self, auto_secret: &super::AutoSecret) -> Result<Secret, ControllerError> {
+    let oref = auto_secret.controller_owner_ref(&()).unwrap();
+    let name = auto_secret.name()?;
+    let namespace = auto_secret.namespace()?;
+
+    let secret_api = Api::<Secret>::namespaced(self.clone(), &namespace);
+    let mut secret = Secret {
+      metadata: ObjectMeta {
+        name: Some(name.clone()),
+        owner_references: Some(vec![oref]),
+        ..ObjectMeta::default()
+      },
+      ..Default::default()
+    };
+
+    let existing_secret = get_secret(&secret_api, &name).await?;
+    if let Some(existing_secret) = existing_secret {
+      secret.metadata.annotations = existing_secret.metadata.annotations.map(|mut annotations| {
+        annotations.retain(|k, _| k.starts_with(ANNOTATION_PREFIX));
+        annotations
+      });
+
+      secret.data = existing_secret.data;
+    }
+
+    Ok(secret)
+  }
+}
+
+#[async_trait::async_trait]
+pub trait AutoSecretExt {
+  fn namespace(&self) -> Result<String, ControllerError>;
+  fn name(&self) -> Result<String, ControllerError>;
+  fn secrets(&self) -> HashMap<String, super::AutoSecretType>;
+}
+
+#[async_trait::async_trait]
+impl AutoSecretExt for super::AutoSecret {
+  fn namespace(&self) -> Result<String, ControllerError> {
+    self
+      .metadata
+      .namespace
+      .clone()
+      .ok_or(ControllerError::MissingObjectKey(".metadata.namespace"))
+  }
+
+  fn name(&self) -> Result<String, ControllerError> {
+    self
+      .metadata
+      .name
+      .clone()
+      .ok_or(ControllerError::MissingObjectKey(".metadata.name"))
+  }
+
+  fn secrets(&self) -> HashMap<String, super::AutoSecretType> {
+    self.spec.secrets.clone()
+  }
+}
+
+pub enum SecretStatus {
+  Missing,
+  Outdated,
+  Matches,
+}
+
+#[async_trait::async_trait]
+pub trait SecretExt {
+  fn retain(&mut self, filter: impl FnMut(&str, &ByteString) -> bool);
+  fn secret_status(&self, name: &str, spec: &super::AutoSecretType) -> SecretStatus;
+  fn set_secret(&mut self, name: &str, spec: &super::AutoSecretType);
+  async fn apply(self, client: Client) -> Result<(), ControllerError>;
+}
+
+#[async_trait::async_trait]
+impl SecretExt for Secret {
+  fn retain(&mut self, filter: impl FnMut(&str, &ByteString) -> bool) {
+    let annotations = self.metadata.annotations.get_or_insert_with(Default::default);
+    let data = self.data.get_or_insert_with(Default::default);
+
+    let to_remove = data
+      .iter()
+      .filter(|(n, v)| filter(*n, *v))
+      .map(|(n, _)| n)
+      .cloned()
+      .collect::<Vec<_>>();
+
+    for name in to_remove {
+      remove_secret(annotations, data, &*name);
+    }
+  }
+
+  fn secret_status(&self, name: &str, spec: &super::AutoSecretType) -> SecretStatus {
+    let annotations = match self.metadata.annotations.as_ref() {
+      None => return SecretStatus::Missing,
+      Some(v) => v,
+    };
+
+    let annotation_name = annotation_name(name);
+    let expected_hash = annotations.get(&annotation_name).cloned();
+    let actual_hash = hash(spec);
+
+    match expected_hash {
+      Some(expected) if expected == actual_hash => SecretStatus::Matches,
+      Some(_) => SecretStatus::Outdated,
+      None => SecretStatus::Missing,
+    }
+
+    // let value = ByteString(conf.generate().into_bytes());
+    // annotations.insert(annotation_name, actual_hash);
+    // data.insert(name.into(), value);
+  }
+
+  fn set_secret(&mut self, name: &str, spec: &super::AutoSecretType) {
+    let annotations = self.metadata.annotations.get_or_insert_with(Default::default);
+    let data = self.data.get_or_insert_with(Default::default);
+    let value = ByteString(spec.generate().into_bytes());
+    let annotation_name = annotation_name(name);
+    let actual_hash = hash(spec);
+
+    annotations.insert(annotation_name, actual_hash);
+    data.insert(name.into(), value);
+  }
+
+  async fn apply(self, client: Client) -> Result<(), ControllerError> {
+    let namespace = self.metadata.namespace.clone().unwrap();
+    let name = self.metadata.name.clone().unwrap();
+    let secret_api = Api::<Secret>::namespaced(client, &namespace);
+
+    patch_secret(secret_api, &name, self).await
+  }
+}
+
+const ANNOTATION_PREFIX: &str = "autosecrets.webstep.no/";
+
+fn annotation_name(name: &str) -> String {
+  format!("{ANNOTATION_PREFIX}{name}")
+}
+
+#[tracing::instrument(skip_all, fields(secret.name = name))]
+async fn get_secret(secret_api: &Api<Secret>, name: &str) -> Result<Option<Secret>, ControllerError> {
+  secret_api.get_opt(name).await.map_err(ControllerError::SecretGetFailed)
+}
+
+#[tracing::instrument(skip_all, fields(secret.name = name))]
+async fn patch_secret(secret_api: Api<Secret>, name: &str, secret: Secret) -> Result<(), ControllerError> {
+  secret_api
+    .patch(
+      name,
+      &PatchParams::apply("autosecrets.webstep.no").force(),
+      &Patch::Apply(&secret),
+    )
+    .await
+    .map_err(ControllerError::SecretApplyFailed)?;
+
+  Ok(())
+}
+
+fn remove_secret(annotations: &mut BTreeMap<String, String>, data: &mut BTreeMap<String, ByteString>, name: &str) {
+  info!("removing secret {}", name);
+  annotations.remove(&annotation_name(name));
+  data.remove(name);
+}
+
+fn hash(value: &impl Hash) -> String {
+  let mut hasher = seahash::SeaHasher::new();
+  value.hash(&mut hasher);
+  let value = hasher.finish();
+  hex::encode(value.to_le_bytes())
+}
+
+#[derive(Debug, Error)]
+pub enum ControllerError {
+  #[error("Failed to get secret: {0}")]
+  SecretGetFailed(#[source] kube::Error),
+
+  #[error("Failed to apply secret: {0}")]
+  SecretApplyFailed(#[source] kube::Error),
+
+  #[error("MissingObjectKey: {0}")]
+  MissingObjectKey(&'static str),
+}
